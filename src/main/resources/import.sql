064ad6174557acacd5d7a2b7509f461456ecf9c0
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-21 23:59:26 -0400

    Script sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_departamento(nome) VALUES ('Gestão');
+INSERT INTO tb_departamento(nome) VALUES ('Informática');
+
+INSERT INTO tb_usuario(departamento_id, nome, email) VALUES (1, 'Lucas', 'lucas@gmail.com');
+INSERT INTO tb_usuario(departamento_id, nome, email) VALUES (1, 'Diego', 'diego@gmail.com');
+INSERT INTO tb_usuario(departamento_id, nome, email) VALUES (2, 'Silva', 'silva@gmail.com');
+INSERT INTO tb_usuario(departamento_id, nome, email) VALUES (2, 'Santana', 'santana@gmail.com');
