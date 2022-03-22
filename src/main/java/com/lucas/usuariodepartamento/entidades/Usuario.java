fe864f12d8955d215634feaed9aaa07f44434b72
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-21 23:35:33 -0400

    Classes criadas e mapeamento objeto relacional

---
+++ src/main/java/com/lucas/usuariodepartamento/entidades/Usuario.java
@@ -0,0 +1,61 @@
+package com.lucas.usuariodepartamento.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+
+	@ManyToOne
+	@JoinColumn(name = "DEPARTAMENTO_ID")
+	private Departamento departamento;
+
+	Usuario() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+}
